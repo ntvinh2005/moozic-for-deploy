ca0d19c5a98c6d3a1bb1dd54f1cdfd4e6b5ee31f
Vinh Nguyen <ntvinhgv@gmail.com>
2024-07-28 11:39:52 -0400

    Update Modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -14,10 +14,16 @@ const Modal = ({ show, onClose }) => {
           Before using the app, please make sure you are in a well-lit environment and your face is clearly visible to the camera.
         </p>
         <p>
+            Our AI model might need a minute to start so please be patient. Thank you!
+        </p>
+        <p>
           Also make sure the background color contrasts with your skin tone for better detection.
         </p>
         <p>
           Wait for the blue box to constantly capture the face before clicking on the Capture Mood button.
+        </p>
+        <p>
+            Enjoy using our service!
         </p>
         <button onClick={onClose}>Got it!</button>
       </div>
